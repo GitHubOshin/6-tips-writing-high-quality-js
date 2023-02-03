e77ba4c05c41fee093c391c0aef7962b966a6ab3
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-03 23:52:08 +0700

    Update 2-temlate-literals.js

--- 2-temlate-literals.js
+++ 2-temlate-literals.js
@@ -1,1 +1,13 @@
 // 2. Temlate literals
+
+// ❌ BAD 👎
+
+function toString(name, lastname) {
+  return 'Name :' + name + ', LastName :' + lastname
+}
+
+// 💖 GOOD 👍🏻
+
+function toString(name, lastname) {
+  return `Name : ${name}, LastName : ${lastname}`
+}
