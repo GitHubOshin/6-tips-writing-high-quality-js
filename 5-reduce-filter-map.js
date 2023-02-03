128f35018e4d8fc65bdbbbd327a3f56d5f38ab4f
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-03 23:53:36 +0700

    Update 5-reduce-filter-map.js

--- 5-reduce-filter-map.js
+++ 5-reduce-filter-map.js
@@ -1,1 +1,17 @@
 // 5. Reduce, Filter, Map
+
+// ❌ BAD 👎
+
+// const numbers = [1, 2, 3]
+// let sum = 0
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i]
+// }
+
+// 💖 GOOD 👍🏻
+
+const numbers = [1, 2, 3]
+
+const sum = numbers.reduce((total, value) => total + value, 0)
+const evens = numbers.filter((value) => value % 2 === 0)
+const square = numbers.map((value) => value ** 2)
