04ff5a5d35da163d65fc7264997480c00b04ab2c
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-03 23:51:38 +0700

    Update 1-default-parameters.js

--- 1-default-parameters.js
+++ 1-default-parameters.js
@@ -1,1 +1,25 @@
 // 1. Default parameters
+
+const API = {
+  getWeather: () => 'Sunny'
+}
+
+// ❌ BAD 👎
+
+// function getWeather(cityName) {
+//   return API.getWeather(cityName || 'Bangkok')
+// }
+
+// 💖 GOOD 👍🏻
+
+// function getWeather(cityName = 'Bangkok') {
+//   return API.getWeather(cityName)
+// }
+
+function getWeather(cityName = requireCityName()) {
+  return API.getWeather(cityName)
+}
+
+function requireCityName() {
+  throw new Error('cityName parameter is required')
+}
