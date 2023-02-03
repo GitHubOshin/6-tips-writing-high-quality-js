e1140875946edd54df55585568f373eb342c7295
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-03 23:53:08 +0700

    Update 4-spread.js

--- 4-spread.js
+++ 4-spread.js
@@ -1,1 +1,21 @@
 // 4. Spread
+
+// ❌ BAD 👎
+
+// const animal = { name: 'pig' }
+// animal.colour = 'pink'
+
+// 💖 GOOD 👍🏻
+
+const animal = { name: 'pig' }
+animal = { ...animal, colour: 'pink' }
+
+// ❌ BAD 👎
+
+// const food = ['🍣 ', '🍕', '🍰', '🍚']
+// food.push('💖')
+
+// 💖 GOOD 👍🏻
+
+const food = ['🍣 ', '🍕', '🍰', '🍚']
+food = [...food, '💖']
