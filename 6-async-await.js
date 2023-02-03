7557e625fb8f80cb38b61e189e10bf6bf6ba974d
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-02-03 23:54:04 +0700

    Update 6-async-await.js

--- 6-async-await.js
+++ 6-async-await.js
@@ -1,1 +1,25 @@
 // 6 Async/Await
+
+const API = {
+  getWeather: () => Promise.resolve(),
+  process: () => Promise.resolve()
+}
+
+// ❌ BAD 👎
+
+// function getWeather() {
+//   return API.getWeather()
+//     .then((weather) => {
+//       return API.process(weather)
+//     })
+//     .then((weather) => {
+//       console.log(`Weather is ${weather}`)
+//     })
+// }
+
+// 💖 GOOD 👍🏻
+
+async function getWeather() {
+  const weather = await API.getWeather()
+  console.log(`Weather is ${await API.process(weather)}`)
+}
